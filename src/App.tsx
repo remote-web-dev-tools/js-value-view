1fe9fcde0525091fb8af3dc592601dd7904177ff
lleohao <lleohao@hotmail.com>
2021-01-15 00:58:17 +0800

    feat: support atom value view
    #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import JsValueView from './js-value-view/js-value-view';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <h1 className={styles.title}>Js Value View</h1>
+
+      <section>
+        <header>Number</header>
+        <article>
+          <JsValueView value={1} />
+          <JsValueView value={1.12} />
+          <JsValueView value={Number.MAX_SAFE_INTEGER} />
+          <JsValueView value={Number.MAX_VALUE} />
+          <JsValueView value={Number.MIN_SAFE_INTEGER} />
+          <JsValueView value={Number.MIN_VALUE} />
+        </article>
+      </section>
+
+      <section>
+        <header>Boolean</header>
+        <article>
+          <JsValueView value={true} />
+          <JsValueView value={false} />
+        </article>
+      </section>
+
+      <section>
+        <header>null & undefined</header>
+        <article>
+          <JsValueView value={null} />
+          <JsValueView value={undefined} />
+        </article>
+      </section>
+
+      <section>
+        <header>string</header>
+        <article>
+          <JsValueView value={'null'} />
+          <JsValueView value={'undefined'} />
+          <JsValueView value={'Hello World'} />
+        </article>
+      </section>
     </div>
   );
 }
