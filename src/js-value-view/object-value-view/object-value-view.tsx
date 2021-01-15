40687fc543c8beaa0891548efac47d66b07d1c02
lleohao <lleohao@hotmail.com>
2021-01-15 14:43:01 +0800

    feat: support object value view

---
+++ src/js-value-view/object-value-view/object-value-view.tsx
@@ -0,0 +1,90 @@
+import React, { useMemo, useState, MouseEvent } from 'react';
+import styles from './object-value-view.module.scss';
+import classnames from 'classnames';
+import JsValueView from '../js-value-view';
+
+export interface ObjectViewProps {
+  value: Record<string, any>;
+  summary?: boolean;
+  disabledExpand?: boolean;
+}
+
+const ObjectValueView = (props: ObjectViewProps) => {
+  const { value, disabledExpand, summary } = props;
+  const [showDetails, setShowDetails] = useState(false);
+  const [keys, setKeys] = useState(Object.keys(value));
+
+  const handleShowDetails = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+
+    if (disabledExpand) return;
+
+    setShowDetails((prevState) => !prevState);
+  };
+
+  const handleClickDetails = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  /* display: {a: 1, b: {...}, c: Array(3)} */
+  const InlineView = useMemo(
+    () => (
+      <div className={styles.inlineView}>
+        <span>{'{'}</span>
+        {keys.map((key, i) => (
+          <span key={key} className={styles.inlineViewItem}>
+            <span className={styles.key}>{key}</span>
+            <span className={styles.colon}>:</span>
+            <span className={'value'}>
+              <JsValueView value={value[key]} summary disabledExpand />
+            </span>
+            {i !== keys.length - 1 ? <span className={styles.comma}>,</span> : null}
+          </span>
+        ))}
+        <span>{'}'}</span>
+      </div>
+    ),
+    [value]
+  );
+
+  /* display: {...} */
+  const SummaryView = useMemo(() => <div className={'summary-view'}>{'{...}'}</div>, [value]);
+
+  /**
+   * display:
+   *   a: 1,
+   *   b: {a: 1},
+   *   c: (3) [1, 2, 3]
+   */
+  const DetailsView = useMemo(
+    () => (
+      <div className={styles.detailsView} onClick={handleClickDetails}>
+        {keys.map((key) => (
+          <div key={key} className={styles.detailsViewItem}>
+            <span className={styles.key}>{key}</span>
+            <span className={styles.colon}>:</span>
+            <span className={'value'}>
+              <JsValueView value={value[key]} />
+            </span>
+          </div>
+        ))}
+      </div>
+    ),
+    [value]
+  );
+
+  return (
+    <div
+      className={classnames(['object-value', styles.wrap], {
+        [styles.disabledShowDetails]: disabledExpand,
+        [styles.showDetails]: showDetails,
+      })}
+      onClick={handleShowDetails}
+    >
+      {summary ? SummaryView : InlineView}
+      {showDetails ? DetailsView : null}
+    </div>
+  );
+};
+
+export default ObjectValueView;
